6174b907a9e7e2e66b5a3639ba5b8de2f02a06af
timmlaxton <timmlaxton@gmail.com>
2023-02-03 13:21:38 +0000

    Created controller Post

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,1 +1,26 @@
 import Post from "../models/Post.js";
+
+export const createPost = async (req, res) => {
+  try {
+    const { userId, description, picturepath } = req.body;
+    const user = await User.findById(userId);
+    const newPost = new Post({
+      userId,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      location: user.location,
+      description,
+      userPicturePath: user.userPicturePath,
+      picturePath,
+      likes: {},
+      comments: [],
+    });
+    await newPost.save();
+
+    const post = await Post.find();
+
+    res.status(201).json(post);
+  } catch (error) {
+    res.status(409).json({ message: err.message });
+  }
+};
