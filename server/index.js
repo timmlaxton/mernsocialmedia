f9289875eba4c12a5c53771d6a5164c6b11dd023
timmlaxton <timmlaxton@gmail.com>
2023-02-03 11:51:13 +0000

    Added user route

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
 import { register } from "./controllers/auth.js";
 
 /* Middleware Config */
@@ -42,6 +43,7 @@ const upload = multer({ storage });
 app.post("/auth/register", upload.single("picture"), register);
 
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 
 /* BE Setup */
 
