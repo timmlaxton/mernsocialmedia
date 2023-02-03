327a639063ba4dcfa56fad30980a0b8c85d32146
timmlaxton <timmlaxton@gmail.com>
2023-02-03 11:07:20 +0000

    Created model, controller and added assets

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,37 @@
 import brcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
+
+export const register = async (req, res) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      picturePath,
+      friends,
+      location,
+      occupation,
+    } = req.body;
+
+    const salt = await brcrypt.genSalt();
+    const passwordHash = await brcrypt.hash(password, salt);
+    const newuser = new User({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+      picturePath,
+      friends,
+      location,
+      occupation,
+      viewedProfile: Math.floor(Math.random() * 1000),
+      imporessiokns: Math.floor(Math.random() * 1000),
+    });
+    const saveUser = await newuser.save();
+    res.status(201).json(savedUser);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
--- server/models/User.js
+++ server/models/User.js
@@ -1,10 +1,45 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoosse.Schema({
-  firtName: {
-    type: String,
-    required: true,
-    min: 2,
-    max: 50,
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 5,
+      max: 50,
+    },
+    picturePath: {
+      type: String,
+      default: "",
+    },
+    friends: {
+      type: Array,
+      default: [],
+    },
+    location: String,
+    occupation: String,
+    viewedProfile: Number,
+    impressions: Number,
   },
-});
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", UserSchema);
+export default User;
