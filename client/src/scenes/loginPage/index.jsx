1af6c1d913a0c873620569c8bbbf3c6bc823411b
timmlaxton <timmlaxton@gmail.com>
2023-02-12 15:57:30 +0000

    Create login page

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,7 +1,9 @@
 import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
 
 const LoginPage = () => {
-  return <div>Login Page</div>;
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+  return <Box>Login Page</Box>;
 };
 
 export default LoginPage;
